4c60beb62079470ebc394f68c39fca0907ed493d
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2014-11-14 07:42:12 +0700

    add final solution for pascals-triangle

--- leetcode/pascals-triangle.cpp
+++ leetcode/pascals-triangle.cpp
@@ -1,32 +1,29 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int>> outside;
+        if (numRows<=0) {
+            return outside;
+        }
+        else{
 
-int main() {
-  int numRows;
-  //int temp=1;
-  vector<int> inside, lastInside;
-  cin >> numRows;
-  for (int i=0;i<numRows;i++) {
+            outside.resize(numRows);
+            vector<int> inside, lastInside;
+            for (int i = 0; i < numRows; i++) {
+                inside.resize(i+1);
+                for (int j=0; j < i+1; j++) {
+                    if ((j==0)||(j==i)) {
+                        inside[j]=1;
+                    }
+                    else {
+                        inside[j] = lastInside[j] + lastInside[j-1];
+                    }
+                }
+                outside[i]=inside;
+                lastInside=inside;
 
-    inside.resize(i+1);
-    //PRINT INSIDE VECTORS HERE
-    for (int j=0;j<i+1;j++) {
-      if ((j==0)||(j==i)) {
-        cout << 1 << " ";
-        inside[j] = 1;
-      }
-      else {
-        inside[j]=lastInside[j]+lastInside[j-1];
-        cout << inside[j] << " ";
-      }
+            }
+            return outside;
+        }
     }
-    cout << endl;
-    //INSIDE VECTOR END
-    lastInside=inside;
-  }
-  return 0;
-}
+};
